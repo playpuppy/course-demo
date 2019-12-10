8b35a6b829d0fa5d8e464b8d4f7dacdce4663001
Yuka Akinobu <sx2_vn_yuka@outlook.jp>
2019-12-10 14:13:21 +0900

    added demo(Domino)

--- course/TronShow/Rectangle2/sample.py
+++ course/TronShow/Rectangle2/sample.py
@@ -1,5 +1,9 @@
+from puppy2d import *
+
 setGravity(0, -1)
+
 Rectangle(0, -500, 1100, 50, isStatic=True)
+
 for j in range(10):
     for i in range(-500, 501, 100):
         Rectangle(i, 400, 100, 100)
